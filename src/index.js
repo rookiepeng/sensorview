d632570443279f870cee7ccd3438701d809c37e9
rookiepeng <rookiepeng@gmail.com>
2019-06-25 22:05:11 -0400

    wordview example

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import Example from "./Example";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function App() {
+  return (
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <Example />
+    </div>
+  );
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
