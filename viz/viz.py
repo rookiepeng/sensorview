457a2eb02848317732885999e00a9afeb0894c79
Zhengyu Peng <rookiepeng@users.noreply.github.com>
2021-03-13 20:00:08 -0500

    add white outline

--- viz/viz.py
+++ viz/viz.py
@@ -91,7 +91,7 @@ def get_figure_data(det_list,
                 cmin=c_range[0],
                 cmax=c_range[1],
                 line=dict(
-                    # color='darkblue',
+                    color='white',
                     width=1
                 )
             ),
