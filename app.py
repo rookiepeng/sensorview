a16dc9a253c620be5b725302b093f91b3ce61fd3
Zhengyu Peng <rookiepeng@users.noreply.github.com>
2021-03-23 10:11:57 -0400

    Update app.py

--- app.py
+++ app.py
@@ -808,6 +808,7 @@ def data_file_selection(
             ui_config['numerical']
             [ui_config['slider']]['key']].unique()
         frame_idx = frame_idx.reset_index()
+        print(frame_idx)
         redis_instance.set(
             REDIS_KEYS["FRAME_IDX"]+session_id,
             context.serialize(frame_idx).to_buffer().to_pybytes(),
@@ -996,7 +997,7 @@ def update_filter(
         # filterd_frame = filterd_frame.reset_index()
         filterd_frame = context.deserialize(redis_instance.get(
             "FRAME"+session_id+str(frame_idx[slider_arg])))
-
+        print("FRAME"+session_id+str(frame_idx[slider_arg]))
         filterd_frame = filter_all(
             filterd_frame,
             num_keys,
