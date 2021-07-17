01922daba7bce477f3e6a541c1fbd7894aea886e
Zhengyu Peng <rookiepeng@users.noreply.github.com>
2021-07-17 14:25:27 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 celery -A tasks worker --loglevel=info -P gevent &
-gunicorn --timeout=600 --workers=5 --threads=2 -b 0.0.0.0:8000 app:server
+gunicorn --timeout=600 --workers=5 --threads=2 -b 0.0.0.0:8000 app:server &
