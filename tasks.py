84cb4fcb76129c14f16e117de1571abd21ae578e
rookiepeng <rookiepeng@users.noreply.github.com>
2021-07-17 08:47:17 -0400

    clean

--- tasks.py
+++ tasks.py
@@ -1,13 +1,42 @@
+"""
+
+    Copyright (C) 2019 - PRESENT  Zhengyu Peng
+    E-mail: zpeng.me@gmail.com
+    Website: https://zpeng.me
+
+    `                      `
+    -:.                  -#:
+    -//:.              -###:
+    -////:.          -#####:
+    -/:.://:.      -###++##:
+    ..   `://:-  -###+. :##:
+           `:/+####+.   :##:
+    .::::::::/+###.     :##:
+    .////-----+##:    `:###:
+     `-//:.   :##:  `:###/.
+       `-//:. :##:`:###/.
+         `-//:+######/.
+           `-/+####/.
+             `+##+.
+              :##:
+              :##:
+              :##:
+              :##:
+              :##:
+               .+:
+
+"""
+
 from celery import Celery
 from celery.utils.log import get_task_logger
 import redis
 import os
 import base64
+import pickle
+import pandas as pd
 
-from filter import filter_all
 from viz.viz import get_scatter3d
 
-import pickle
 
 EXPIRATION = 172800  # a week in seconds
 
@@ -19,7 +48,46 @@ logger = get_task_logger(__name__)
 celery_app = Celery("Celery_App", broker=os.environ.get(
     'REDIS_URL', 'redis://127.0.0.1:6379'))
 
-accept_content = ['json']
+
+def filter_all(
+        data,
+        num_list,
+        num_values,
+        cat_list,
+        cat_values,
+        vis_table=None,
+        vis_list=None,
+):
+
+    for f_idx, f_name in enumerate(num_list):
+        if f_idx == 0:
+            condition = (data[f_name] >= num_values[f_idx][0]) \
+                & (data[f_name] <= num_values[f_idx][1])
+        else:
+            condition = condition \
+                & (data[f_name] >= num_values[f_idx][0]) \
+                & (data[f_name] <= num_values[f_idx][1])
+
+    for f_idx, f_name in enumerate(cat_list):
+        if not cat_values[f_idx]:
+            condition = condition & False
+            break
+        else:
+            for val_idx, val in enumerate(cat_values[f_idx]):
+                if val_idx == 0:
+                    val_condition = data[f_name] == val
+                else:
+                    val_condition = val_condition \
+                        | (data[f_name] == val)
+
+            condition = condition & val_condition
+
+    if len(vis_list) == 1:
+        condition = condition & (vis_table['_VIS_'] == vis_list[0])
+    elif not vis_list:
+        condition = condition & False
+
+    return data.loc[condition]
 
 
 @celery_app.task(bind=True)
@@ -45,15 +113,13 @@ def celery_filtering_data(self,
                           z_range,
                           c_range):
 
-    logger.info('ID:'+str(self.request.id))
-
     task_id = self.request.id
 
     redis_instance.set(
-            'TASKID'+session_id,
-            pickle.dumps(self.request.id),
-            ex=EXPIRATION
-        )
+        'TASKID'+session_id,
+        pickle.dumps(self.request.id),
+        ex=EXPIRATION
+    )
 
     vis_table = pickle.loads(redis_instance.get("VIS"+session_id))
     frame_idx = pickle.loads(redis_instance.get("FRAME_IDX"+session_id))
@@ -112,7 +178,7 @@ def celery_filtering_data(self,
             ref_name='Host Vehicle'
         )
 
-        if pickle.loads(redis_instance.get('TASKID'+session_id))==task_id:
+        if pickle.loads(redis_instance.get('TASKID'+session_id)) == task_id:
             redis_instance.set(
                 'FIG'+session_id+str(slider_arg),
                 pickle.dumps(fig),
@@ -124,5 +190,5 @@ def celery_filtering_data(self,
                 ex=EXPIRATION
             )
         else:
+            logger.info('Task '+str(task_id)+' terminated by a new task.')
             return
-    # return
