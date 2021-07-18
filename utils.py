cab6b5fa31dbac332be43e76f66c3a7f39ff4849
rookiepeng <rookiepeng@users.noreply.github.com>
2021-07-18 10:24:35 -0400

    update

---
+++ utils.py
@@ -0,0 +1,36 @@
+"""
+
+    Copyright (C) 2019 - PRESENT  Zhengyu Peng
+    E-mail: zpeng.me@gmail.com
+    Website: https://zpeng.me
+
+    `                      `
+    -:.                  -#:
+    -//:.              -###:
+    -////:.          -#####:
+    -/:.://:.      -###++##:
+    ..   `://:-  -###+. :##:
+           `:/+####+.   :##:
+    .::::::::/+###.     :##:
+    .////-----+##:    `:###:
+     `-//:.   :##:  `:###/.
+       `-//:. :##:`:###/.
+         `-//:+######/.
+           `-/+####/.
+             `+##+.
+              :##:
+              :##:
+              :##:
+              :##:
+              :##:
+               .+:
+
+"""
+
+
+import json
+
+
+def load_config(json_file):
+    with open(json_file, 'r') as read_file:
+        return json.load(read_file)
