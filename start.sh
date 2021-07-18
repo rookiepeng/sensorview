2dd0a6ecacb9674c183c52b40f8cc1bb4338def6
rookiepeng <rookiepeng@users.noreply.github.com>
2021-07-18 10:11:38 -0400

    clean

--- start.sh
+++ start.sh
@@ -1,4 +1,30 @@
 #!/bin/sh
 
+# Copyright (C) 2019 - PRESENT  Zhengyu Peng
+# E-mail: zpeng.me@gmail.com
+# Website: https://zpeng.me
+
+# `                      `
+# -:.                  -#:
+# -//:.              -###:
+# -////:.          -#####:
+# -/:.://:.      -###++##:
+# ..   `://:-  -###+. :##:
+#        `:/+####+.   :##:
+# .::::::::/+###.     :##:
+# .////-----+##:    `:###:
+#  `-//:.   :##:  `:###/.
+#    `-//:. :##:`:###/.
+#      `-//:+######/.
+#        `-/+####/.
+#          `+##+.
+#           :##:
+#           :##:
+#           :##:
+#           :##:
+#           :##:
+#            .+:
+
+
 celery -A tasks worker --loglevel=info -P gevent &
 gunicorn --timeout=600 --workers=5 --threads=2 -b 0.0.0.0:8000 app:server
