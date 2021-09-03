2ca83354ee917b1aaf4deda4cb95a69690bbf43c
Zhengyu Peng <rookiepeng@users.noreply.github.com>
2021-09-03 14:41:32 -0400

    Update init_sensorview.bat

--- kube/init_sensorview.bat
+++ kube/init_sensorview.bat
@@ -2,14 +2,15 @@
 
 ECHO Initialize minikube
 
-minikube start --mount-string "D:\:/mnt/data" --mount=true
-@REM minikube start
+@REM minikube start --mount-string "D:\:/mnt/data" --mount=true
+minikube start
 
 kubectl apply -f redis-server.yaml
 kubectl expose deployment/redis-server
 
 START "" minikube mount "D:\:/mnt/data"
 
+@REM minikube ismage load sensorview.tar
 kubectl apply -f sensorview-server.yaml
 kubectl expose deployment/sensorview-server
 
