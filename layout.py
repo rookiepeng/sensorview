8c9492f2c2c142c9cda3520055bbcf6da4d61677
Zhengyu Peng <rookiepeng@users.noreply.github.com>
2021-11-22 16:15:22 -0500

    update

--- layout.py
+++ layout.py
@@ -796,4 +796,4 @@ def get_app_layout(app):
                 [Redis](https://redis.io/),\
                 [Celery](https://docs.celeryproject.org/en/stable/),\
                 [Docker](https://www.docker.com/)'),
-    ], fluid=True, className="dbc_pulse")
+    ], fluid=True, className="dbc_light")
