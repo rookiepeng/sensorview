5628d9f555dac3938c8c53583304bf5a02cda750
Zhengyu Peng <rookiepeng@users.noreply.github.com>
2022-03-27 13:59:59 -0400

    Revert "Revert "stop playing when cache is not ready""
    This reverts commit 7135b685d54bed1f808a644df927e12c4e0c8bbf.

--- views/test_case_view.py
+++ views/test_case_view.py
@@ -412,7 +412,8 @@ def file_select_changed(
         State('file-picker', 'value'),
         State('case-picker', 'value'),
         State('slider-frame', 'max'),
-        State('slider-frame', 'value')
+        State('slider-frame', 'value'),
+        State('session-id', 'data')
     ])
 def update_slider(
         file_loaded,
@@ -423,6 +424,7 @@ def update_slider(
         case,
         slider_max,
         slider_var,
+        session_id
 ):
     """
     Callback when a data file is selected
@@ -493,6 +495,11 @@ def update_slider(
         # triggerred from interval
         if slider_var == slider_max:
             return [dash.no_update]
+
+        fig_idx = cache_get(session_id, CACHE_KEYS['figure_idx'])
+        if fig_idx is not None:
+            if slider_var > fig_idx:
+                return [dash.no_update]
 
         else:
             return [(slider_var+1) % (slider_max+1)]
