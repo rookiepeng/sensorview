26b3184d85e3e2a50316a62205a1f315217e0ca0
Zhengyu Peng <rookiepeng@users.noreply.github.com>
2022-03-28 16:27:25 -0400

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -189,6 +189,8 @@ def celery_filtering_data(
 
     if cache_get(session_id, CACHE_KEYS['task_id']) != task_id:
         logger.info('Task '+str(task_id)+' terminated by a new task')
+        del frame_group
+        del dataset
         return
 
     # set graph's range the same for all the frames
@@ -305,6 +307,9 @@ def celery_filtering_data(
                       CACHE_KEYS['figure_idx'])
         else:
             logger.info('Task '+str(task_id)+' terminated by a new task')
+            del data
+            del frame_group
+            del dataset
             return
 
 
