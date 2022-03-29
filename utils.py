fc3cd0a8573246daaa5fd3925d9668e44b951207
Zhengyu Peng <rookiepeng@users.noreply.github.com>
2022-03-28 21:51:29 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -87,13 +87,13 @@ def cache_set(data, id, key_major, key_minor=None):
         key_str = key_major+id
     else:
         key_str = key_major+id+key_minor
-    # redis_instance.set(
-    #     key_str,
-    #     pickle.dumps(data),
-    #     ex=EXPIRATION
-    # )
+    redis_instance.set(
+        key_str,
+        pickle.dumps(data),
+        ex=EXPIRATION
+    )
     # cache.pop(key_str)
-    cache.set(key_str, data, expire=EXPIRATION)
+    # cache.set(key_str, data, expire=EXPIRATION)
 
 
 def redis_set(data, id, key_major, key_minor=None):
@@ -140,14 +140,14 @@ def cache_get(id, key_major, key_minor=None):
     else:
         key_str = key_major+id+key_minor
 
-    # val = redis_instance.get(key_str)
-    val = cache.get(key_str, default=None, retry=True)
-    return val
+    val = redis_instance.get(key_str)
+    # val = cache.get(key_str, default=None, retry=True)
+    # return val
 
-    # if val is not None:
-    #     return pickle.loads(val)
-    # else:
-    #     return None
+    if val is not None:
+        return pickle.loads(val)
+    else:
+        return None
 
 
 def redis_get(id, key_major, key_minor=None):
