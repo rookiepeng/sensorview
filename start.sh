340604d39bb7b037ee6160e11314a58b0fe714e5
Zhengyu Peng <rookiepeng@users.noreply.github.com>
2022-10-14 14:46:53 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -25,6 +25,6 @@
 #           :##:
 #            .+:
 
-# prefork works on Ubuntu on VirtualBox
+
 celery -A tasks worker --loglevel=info --pool=prefork --max-tasks-per-child=1 &
 gunicorn --timeout=600 --workers=5 --threads=2 -b 0.0.0.0:8000 app:server
