ce9a03b7be296ba8ec8e9e5d875de92ec2608caf
Zhengyu Peng <rookiepeng@users.noreply.github.com>
2022-10-14 14:46:50 -0400

    Delete start_standalone.bat

--- start_standalone.bat
+++
@@ -1,7 +0,0 @@
-
-@REM call activate [conda_env]
-START /b redis-server
-SLEEP 2
-START /b celery -A tasks worker --loglevel=info -P gevent
-SLEEP 2
-START /b waitress-serve --listen=*:8000 app:server
