b497efd0d20e4bf87e9c7c2f754859fd895bd80a
Zhengyu Peng <zpeng@users.noreply.github.com>
2025-10-10 11:33:01 -0400

    Add relayout-data store and callback for 3D scatter view
    Introduces a new dcc.Store for 'relayout-data' in the app layout and adds a callback in scatter_3d_view.py to update this store with the relayoutData from the 3D scatter plot. Also removes an unused conditional in the data length calculation.

--- layouts/app_layout.py
+++ layouts/app_layout.py
@@ -99,6 +99,7 @@ def get_app_layout():
             dcc.Store(id="local-file-selection", storage_type="local"),
             dcc.Store(id="current-file"),
             dcc.Store(id="key-dict"),
+            dcc.Store(id="relayout-data", data=None),
             dcc.Store(id="dark-template", data=pio.templates["plotly_dark"]),
             dcc.Store(id="light-template", data=pio.templates["plotly"]),
             dcc.Store(id="trigger-remote-figure", data=0),
