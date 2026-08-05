6b0c51a027713163984745fa8ebd4a50cc1eaa45
Zhengyu Peng <rookiepeng@users.noreply.github.com>
2026-08-04 21:37:04 -0400

    Update app_config.py

--- app_config.py
+++ app_config.py
@@ -34,7 +34,7 @@ class SafeDiskcacheManager(DiskcacheManager):
 
 
 APP_TITLE = "SensorView"
-APP_VERSION = "v11.8"
+APP_VERSION = "v12.0"
 
 DATA_PATH = "./data"
 FRAME_CACHE_PATH = "./cache/frame"
